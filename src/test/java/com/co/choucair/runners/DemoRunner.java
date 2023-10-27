48166713bbdcded59eea98dc57c6adf1013df048
Michael Ferneein Garzon Rodriguez <mgarzonr@choucairtesting.com>
2023-10-27 16:11:01 -0500

    ProyectoBaseSPCandidato

---
+++ src/test/java/com/co/choucair/runners/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.co.choucair.runners;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/demo.feature",
+        glue = "com.co.choucair.stepdefinitions",
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class DemoRunner {
+}
---
+++ src/test/resources/features/demo.feature
@@ -0,0 +1,6 @@
+Feature: login on the site
+
+Scenario: login sucessfully
+Given the user is on the site
+When the user types the info to login
+Then the user will see the text
